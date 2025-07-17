5326c9883bd5de89fed9c040f067ed1edef2b7b2
Yash-Jadon <yashjadon@diamondore.in>
2025-07-17 16:47:41 +0530

    folder structure setiup

---
+++ server/middlewares/zod.validator.js
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+const validate = (schemas) => (req, res, next) => {
+  try {
+      
+      const validatedData = {};
+
+      
+      if(schemas.body){ 
+        validatedData.body = schemas.body.parse(req.body);
+      }
+      if(schemas.params){
+        validatedData.params = schemas.params.parse(req.params);
+      }
+      if(schemas.query){
+        validatedData.query = schemas.query.parse(req.query);
+      }
+      if(schemas.file) {   
+        validatedData.file = schemas.file.parse(req.file);       
+      }
+      if(schemas.files) {       
+        validatedData.files = schemas.files.parse(req.files);
+      }    
+      req.validatedData = validatedData;
+      next(); 
+  } catch (error) {
+      console.log(error)
+      if (error instanceof z.ZodError) {
+          return res.status(400).json({
+              success: false,
+              errors: error.errors.map(err => ({
+                  field: err.path[0],
+                  message: err.message
+              }))
+          });
+      }
+      next(error); 
+  }
+};
+
+export default validate;
