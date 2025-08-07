83c6702debe71fc04701088e465acccbc5bcbc46
Zoya774 <zoya.rasonline@gmail.com>
2025-08-07 16:07:28 +0530

    page not found error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { getUserData } from "./Redux/Slices/authSlice";
 import { getNavbarCartWishlistCount } from "./Redux/Slices/cart";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ShippingPolicy from "./pages/ShippingPolicy";
+import { ErrorPage } from "./pages/ErrorPage";
 
 const About = () => <h2 className="text-2xl text-blue-400">About Page</h2>;
 
@@ -65,6 +66,7 @@ const App = () => {
           />
           <Route path="/checkout" element={<CheckOutPage />} />
         </Route>
+        <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </HomeLayout>
   );
